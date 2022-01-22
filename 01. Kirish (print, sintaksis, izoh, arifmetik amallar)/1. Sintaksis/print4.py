f3ef346b3b308a2a3f106fe5c95e82e9a421b9ff
Oybeklinux <Oybeklinux@gmail.com>
2022-01-22 17:22:40 +0500

    mod md file mavzu 1

---
+++ 01. Kirish (print, sintaksis, izoh, arifmetik amallar)/1. Sintaksis/print4.py
@@ -0,0 +1,28 @@
+"""
+4. Quyidagini ekranga chiqaradigan kod yozing ([Yechim](print4.py))
+
+Chiqish
+
+      ***********
+      *********
+      *******
+      *****
+      ***
+      *
+"""
+# 1-usul
+print("""
+***********
+*********
+*******
+*****
+***
+*
+""")
+# 2-usul
+print("***********")
+print("*********")
+print("*******")
+print("*****")
+print("***")
+print("*")
---
+++ 01. Kirish (print, sintaksis, izoh, arifmetik amallar)/1. Sintaksis/xatolik4.py
@@ -0,0 +1,4 @@
+print("Birinchi dars \n")
+print("\"+\" belgisi ikki sonni qo'shish uchun ishlatiladi.")
+print('print funksiyasi: print("Assalomu " + "alaykum")')
+print("Shuningdek '+' belgisi ikki satrni qo'shish uchun ham ishlatiladi")
