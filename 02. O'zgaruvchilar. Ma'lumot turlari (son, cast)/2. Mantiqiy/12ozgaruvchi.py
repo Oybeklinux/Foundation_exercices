76aa1b900aa454a3725ce64c132ac0c44512aa8b
Oybeklinux <Oybeklinux@gmail.com>
2022-01-26 13:36:34 +0500

    fix: dars 2

---
+++ 02. O'zgaruvchilar. Ma'lumot turlari (son, cast)/2. Mantiqiy/12ozgaruvchi.py
@@ -0,0 +1,13 @@
+"""
+12. Hozir vaqt 14:00. 65 minutdan keyin u 15:05 bo'ladi. 70 minutdan keyin 15:10. Mana shuni dasturini yozing. Foydalanuvchi kiritgan minutdan keyin mazkur 14:00 ga nisbatan vaqtni chiqarsin
+```text
+Hozir soat 14:00
+Minutni kiriting: 400
+400 minutdan keyin vaqt 20:40 bo'ladi
+```
+"""
+hozirgi_soat = 14
+print("Hozir soat ", hozirgi_soat, ":00")
+qiymat = int(input("Minutni kiriting: "))
+soat = qiymat // 60
+minut = qiymat % 60
