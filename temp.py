365ac9a92acf2f75d0c7e50382f7435dd2ed0751
Oybeklinux <Oybeklinux@gmail.com>
2022-02-02 12:16:44 +0500

    amaliyot

--- temp.py
+++ temp.py
@@ -1,9 +1,11 @@
-royhat = ["Otabek" ]
-def register():
-    if input("?") == "ha":
-        royhat.append(input("Ismingiz: "))
-
-register()
-register()
-register()
-print(royhat)
+ustun = 15
+print("Ism".center(ustun), "Sharifi".center(ustun))
+ism = 'Otabek'
+sharifi = "Anvarov"
+print(ism.center(ustun), sharifi.center(ustun))
+ism = 'Anvar'
+sharifi = "Bekzodov"
+print(ism.center(ustun), sharifi.center(ustun))
+ism = 'Sherzod'
+sharifi = "Oybekov"
+print(ism.center(ustun), sharifi.center(ustun))
