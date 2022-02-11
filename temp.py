298c64a52a66dc3b76c9b2427315799a11c16f1c
Oybeklinux <Oybeklinux@gmail.com>
2022-02-11 12:53:25 +0500

    dars 4

--- temp.py
+++ temp.py
@@ -1,5 +1,24 @@
+x0 = 2
+y0 = 6
 
+while True:
+    exist = False
+    x1 = int(input("x1 = "))
+    y1 = int(input("y1 = "))
+    if not (1 <= x1 < 9 and 1 <= y1 < 9):
+        continue
 
-json = 4
-import json
-print(json.loads("[3,4,5]"))
+    n = x1 - x0
+    if n > 0:
+        if x1 == x0 + n and y1 == y0 + n or x1 == x0 + n and y1 == y0 - n:
+            exist = True
+    elif n < 0:
+        n *= -1
+        if x1 == x0 - n and y1 == y0 - n or x1 == x0 - n and y1 == y0 + n:
+            exist = True
+
+    if exist:
+        print("To'g'ri")
+    else:
+        print("Xato")
+
