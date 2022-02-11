a97f9dc93e76dd22af81258a73f9474d6da90acd
Oybeklinux <Oybeklinux@gmail.com>
2022-02-11 11:39:28 +0500

    dars 4

--- temp.py
+++ temp.py
@@ -1,20 +1,5 @@
-# ustun = 15
-# print("Ism".center(ustun), "Sharifi".center(ustun))
-# ism = 'Otabek'
-# sharifi = "Anvarov"
-# print(ism.center(ustun), sharifi.center(ustun))
-# ism = 'Anvar'
-# sharifi = "Bekzodov"
-# print(ism.center(ustun), sharifi.center(ustun))
-# ism = 'Sherzod'
-# sharifi = "Oybekov"
-# print(ism.center(ustun), sharifi.center(ustun))
-soz = "Buxoro shahrining tarixiy markazida"
-alfa = 0
-sign = 0
-for harf in soz:
-    if harf.isalpha():
-        alfa += 1
-    else:
-        sign += 1
-print(f"Harflar soni {alfa}\nBelgilar soni: {sign}")
+
+
+json = 4
+import json
+print(json.loads("[3,4,5]"))
