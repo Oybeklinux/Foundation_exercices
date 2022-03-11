25e1fe814a2fc82a88bfd1daf80781992972fec1
Oybeklinux <Oybeklinux@gmail.com>
2022-03-11 11:44:53 +0500

    fix: 2-dars 13-14 o'rni almashib qolibdi

--- 02. O'zgaruvchilar. Ma'lumot turlari (son, cast)/2. Mantiqiy/ozgaruvchi13.py
+++ 02. O'zgaruvchilar. Ma'lumot turlari (son, cast)/2. Mantiqiy/ozgaruvchi13.py
@@ -1,18 +1,15 @@
 """
-13. Yuqoridagi dasturni universallashtiramiz. Endi mazkur vaqtni ham qo'shiladigan minutni ham foydalanuvchi kiritsin.
+12. Hozir vaqt 14:00. 65 minutdan keyin u 15:05 bo'ladi. 70 minutdan keyin 15:10. Mana shuni dasturini yozing. Foydalanuvchi kiritgan minutdan keyin mazkur 14:00 ga nisbatan vaqtni chiqarsin
 
-Hozirgi vaqt soatini kiriting: 13
-Hozirgi vaqt minutini kiriting: 20
-Qancha minutdan keyin vaqtni bilmoqchisiz? 100
-100 minutdan keyin vaqt 15:00 bo'ladi
+Hozir soat 14:00
+Minutni kiriting: 400
+400 minutdan keyin vaqt 20:40 bo'ladi
 
 """
-h_soat = hozirgi_soat = int(input("Hozirgi vaqt soatini kiriting: "))
-h_minut = hozirgi_minut = int(input("Hozirgi vaqt minutini kiriting: "))
-qiymat = int(input("Qancha minutdan keyin vaqtni bilmoqchisiz? "))
-
-umumiy_minut = hozirgi_minut + qiymat
-hozirgi_soat = hozirgi_soat + umumiy_minut // 60
-hozirgi_minut = umumiy_minut % 60
-
-print(str(h_soat) + ":"+str(h_minut) + " ga " + str(qiymat) + " minut qo'shilsa,  vaqt " + str(hozirgi_soat) + ":" + str(hozirgi_minut) + " bo'ladi")
+hozirgi_soat = 14
+print("Hozir soat " + str(hozirgi_soat) + ":00")
+qiymat = int(input("Minutni kiriting: "))
+soat = qiymat // 60
+minut = qiymat % 60
+hozirgi_soat = hozirgi_soat + soat
+print(qiymat, "minutdan keyin vaqt " + str(hozirgi_soat) + ":" + str(minut) + " bo'ladi")
