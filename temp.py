d8e9929c1457d6204297fa4f83e653cd513dd0ac
Oybeklinux <Oybeklinux@gmail.com>
2022-04-05 14:59:40 +0500

    ex28

--- temp.py
+++ temp.py
@@ -1,13 +1,10 @@
-def daraja_5mi(a):
-    daraja = 1
-    while True:
-        daraja *= 5
-        if daraja == a:
-            return True
-        elif daraja > a:
-            return False
+def fibonachi(n):
 
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonachi(n-1) + fibonachi(n-2)
 
-for i in range(0,1000):
-    if daraja_5mi(i):
-        print(i)
+for i in range(0,10):
+    print(f"{i}-tartibi: {fibonachi(i)}")
