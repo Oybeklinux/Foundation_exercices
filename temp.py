1818f5eebb912c49de6a875f358ad01b8cf960c6
Oybeklinux <Oybeklinux@gmail.com>
2022-04-07 09:34:28 +0500

    fix text

--- temp.py
+++ temp.py
@@ -1,10 +1,24 @@
-def fibonachi(n):
+def fibonachi(tartibi=0):
+    # if a == 0:
+    #     return 0
+    # if a == 1:
+    #     return 1
+    # return fibonachi(a-2) + fibonachi(a-1)
+    sum = 0
+    a1=0
+    a2=1
+    k = [a1, a2]
 
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibonachi(n-1) + fibonachi(n-2)
+    for i in range(2, tartibi+1):
+        sum = a1 + a2
+        a1, a2 = a2, sum
+        k.append(sum)
+        print(k)
+        if i+1 == tartibi:
+            return sum
 
-for i in range(0,10):
-    print(f"{i}-tartibi: {fibonachi(i)}")
+    return k
+
+
+k = int(input("Tartib raqamini kiriting: "))
+print(fibonachi(k))
