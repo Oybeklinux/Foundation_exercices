a8252d079479d1a94f0919b85454692962cb7116
Oybeklinux <Oybeklinux@gmail.com>
2022-04-08 15:12:42 +0500

    ex

--- temp.py
+++ temp.py
@@ -7,11 +7,16 @@ def factorial(n):
 
 def exp(x, n):
     summa = 0
-    for i in range(n):
+    j = 0
+    for i in range(0, n, 2):
+        j += 1
         summa += x ** i / (factorial(i))
-        print(f"qadam-{i + 1}: {x ** i / (factorial(i))}")
-    print(f"Yig'indisi: {exp(5, 3)}")
+        print(f"qadam-{j}: {x ** i / (factorial(i))}")
+    print(f"Yig'indisi: {summa}")
     return summa
 
 
-print(f"Yig'indisi: {exp(5, 3)}")
+x = int(input("x: "))
+n = int(input("Daraja n: "))
+natija = exp(x, n)
+natija = exp(5, 10)
