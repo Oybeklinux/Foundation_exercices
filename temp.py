a706cebe7030ac73a9da35ade78d852e08040b61
Oybeklinux <Oybeklinux@gmail.com>
2022-04-08 16:19:26 +0500

    ex

--- temp.py
+++ temp.py
@@ -10,8 +10,8 @@ def exp(x, n):
     j = 0
     for i in range(0, n, 1):
         j += 1
-        summa += x ** (2*i+1) / (factorial(2*i+1))
-        print(f"qadam-{j}: {x ** (2*i+1) / (factorial(2*i+1))}")
+        summa += ((-1) ** i) * x ** (2*i) / (factorial(2*i))
+        print(f"qadam-{j}: {x ** (2*i) / (factorial(2*i))}")
     print(f"Yig'indisi: {summa}")
     return summa
 
