7ff64aa65a223f54a4185153c6c116ed5d1c1b21
Oybek <Oybeklinux@gmail.com>
2022-11-02 11:01:10 +0500

    update for

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+#  10, 9,8,7,6,5,4,3,2,1
+# 15,25,35,45,55
+# 1 dan 10 gacha sonlarni yig’indisini
+
+matritsa = [[11, 12, 13],
+            [21, 22, 23],
+            [31, 32, 33]]
+
+# print(matritsa[0][1])
+for qator in matritsa:
+    # print(qator)
+    for element in  qator:
+        print(element)
+
+
