4eee32a833f8c01f71991cec8fe914f6ff760be3
Oybek <Oybeklinux@gmail.com>
2022-11-04 09:43:40 +0500

    lesson8

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-#  10, 9,8,7,6,5,4,3,2,1
-# 15,25,35,45,55
-# 1 dan 10 gacha sonlarni yig’indisini
+gap = "non"
 
-matritsa = [[11, 12, 13],
-            [21, 22, 23],
-            [31, 32, 33]]
+#[boshi: ohiri : qadam]
 
-# print(matritsa[0][1])
-for qator in matritsa:
-    # print(qator)
-    for element in  qator:
-        print(element)
-
-
+print(gap[::-1] == gap)
