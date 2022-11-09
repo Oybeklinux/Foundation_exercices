dc68205659a3a9c54abc9e161913f47c9f9aa455
Oybek <Oybeklinux@gmail.com>
2022-11-09 15:43:05 +0500

    sort

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-gap = "non"
+transposed = []
+matrix = [[1, 2, 3, 4], [4, 5, 6, 8]]
 
-#[boshi: ohiri : qadam]
+print(f"Matritsa: {matrix}")
 
-print(gap[::-1] == gap)
+for i in range(len(matrix[0])):
+    transposed_row = []
+
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+print(f"Transponirlangan matritsa: {transposed}")
