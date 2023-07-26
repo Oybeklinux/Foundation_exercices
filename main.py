e713fd9d9b3e743ae6256ec4e7c76f0d3190763a
Oybek <Oybeklinux@gmail.com>
2023-07-26 08:34:54 +0500

    fibonachchi va faktorial

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def mening_funksiyam(royxat_1):
     print("#1:", royxat_1)
     print("#2:", royxat_2)
-    royxat_1 = [0, 1]
+    royxat_1.pop("2")  # Bu yerga ahamiyat bering
     print("#3:", royxat_1)
     print("#4:", royxat_2)
 
 
-royxat_2 = [2, 3]
+royxat_2 = {"2": 3}
 mening_funksiyam(royxat_2)
 print("#5:", royxat_2)
