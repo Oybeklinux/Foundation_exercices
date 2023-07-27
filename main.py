6a1104472674e2e01fcef4bade1d7bb239404c54
Oybek <Oybeklinux@gmail.com>
2023-07-27 12:03:50 +0500

    video yangilandi

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def mening_funksiyam(royxat_1):
-    print("#1:", royxat_1)
-    print("#2:", royxat_2)
-    royxat_1.pop("2")  # Bu yerga ahamiyat bering
-    print("#3:", royxat_1)
-    print("#4:", royxat_2)
-
-
-royxat_2 = {"2": 3}
-mening_funksiyam(royxat_2)
-print("#5:", royxat_2)
+sonlar1 = [1,2,3,4]
+sonlar2 = sonlar1.copy()
+sonlar2 = sonlar1[:]
+sonlar2.pop()
+print(sonlar1)
+print(sonlar2)
